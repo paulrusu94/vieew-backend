0dd3247df1e4ffd776a884335754bbb3ebc43da3
Young Father <paul.rusu94@gmail.com>
2025-03-18 15:38:47 +0200

    Update event name.

--- amplify/functions/scheduler-mining/handler.ts
+++ amplify/functions/scheduler-mining/handler.ts
@@ -17,7 +17,7 @@ export const handler: DynamoDBStreamHandler = async (event) => {
         console.log(`NEW Image:`, newItem);
 
         try {
-          const ruleName = `dynamic-rule-mining-session-${Date.now()}-${newItem!.userId}`;
+          const ruleName = `mining-session-${Date.now()}-${newItem!.userId}`;
 
           const startDate = new Date(newItem!.startDate)
           const endDate = new Date(newItem!.startDate)
